dc6007dc128a8e4ee254802266597332044b1315
Yannik Ache Eicher <ache@bbit.io>
2022-08-18 08:47:04 +0200

    feat(login): autocomplete

--- src/app/session/login/login.component.ts
+++ src/app/session/login/login.component.ts
@@ -10,10 +10,11 @@ import { SessionService } from '../session.service';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  public organization = '';
   public identifier = '';
   public password = '';
-
   public organizations = new BehaviorSubject<IZso[]>([]);
+  public filteredOrganizations = new BehaviorSubject<IZso[]>([]);
 
   constructor(private _session: SessionService, private _api: ApiService) {}
 
@@ -30,10 +31,27 @@ export class LoginComponent {
         orgs.push({ name: org.attributes.name, identifier: org.attributes.users.data[0].attributes?.username });
       }
       this.organizations.next(orgs);
+      this.filteredOrganizations.next(orgs);
     }
   }
 
-  login() {
+  public login(): void {
     this._session.login({ identifier: this.identifier, password: this.password });
+  }
+
+  public organizationSelected(org: IZso): void {
+    this.identifier = org?.identifier;
+    this.organization = org?.name;
+  }
+
+  public filterOrganizations(filter: string): void {
+    if (!filter) {
+      this.filteredOrganizations.next(this.organizations.value);
+      this.organization = '';
+      this.identifier = '';
+      return;
+    } else {
+      this.filteredOrganizations.next(this.organizations.value.filter((org) => org.name?.toLowerCase().includes(filter?.toLowerCase())));
+    }
   }
 }
