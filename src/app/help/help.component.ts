b8dd1f928abcf0ec18a042b2c92e3cb5c7ffb58c
Yannik Ache Eicher <ache@bbit.io>
2022-08-18 10:41:45 +0200

    fix(nl2br): removed pipe since it breaks the angular compiler from time to time

--- src/app/help/help.component.ts
+++ src/app/help/help.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
-import {I18NService} from '../state/i18n.service';
+import { I18NService } from '../state/i18n.service';
 
 @Component({
   selector: 'app-help',
@@ -8,8 +8,5 @@ import {I18NService} from '../state/i18n.service';
   styleUrls: ['./help.component.css'],
 })
 export class HelpComponent {
-  constructor(
-    @Inject(MAT_DIALOG_DATA) public initialLoad: boolean,
-    public i18n: I18NService
-  ) {}
+  constructor(@Inject(MAT_DIALOG_DATA) public initialLoad: boolean, public i18n: I18NService) {}
 }
