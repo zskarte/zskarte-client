09d579984144b15ca8e648c90ff6d4a786e33cd7
Pascal Knecht <p.knecht@e-guma.ch>
2023-01-16 09:18:26 +0100

    Added dropdown instead of autocomplete

--- src/app/session/login/login.component.ts
+++ src/app/session/login/login.component.ts
@@ -10,17 +10,13 @@ import { SessionService } from '../session.service';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  public organization = '';
   public identifier = '';
   public password = '';
   public organizations = new BehaviorSubject<IZso[]>([]);
-  public filteredOrganizations = new BehaviorSubject<IZso[]>([]);
 
   constructor(private _session: SessionService, private _api: ApiService) {}
 
   async ngOnInit() {
-    this.identifier = '';
-    this.password = '';
     const result = await this._api.get(
       '/api/organizations?fields[0]=name&populate[users][fields][0]=username&populate[users][fields][1]=email',
     );
@@ -31,27 +27,10 @@ export class LoginComponent {
         orgs.push({ name: org.attributes.name, identifier: org.attributes.users.data[0].attributes?.username });
       }
       this.organizations.next(orgs);
-      this.filteredOrganizations.next(orgs);
     }
   }
 
   public login(): void {
     this._session.login({ identifier: this.identifier, password: this.password });
-  }
-
-  public organizationSelected(org: IZso): void {
-    this.identifier = org?.identifier;
-    this.organization = org?.name;
-  }
-
-  public filterOrganizations(filter: string): void {
-    if (!filter) {
-      this.filteredOrganizations.next(this.organizations.value);
-      this.organization = '';
-      this.identifier = '';
-      return;
-    } else {
-      this.filteredOrganizations.next(this.organizations.value.filter((org) => org.name?.toLowerCase().includes(filter?.toLowerCase())));
-    }
   }
 }
