fe241be877c86fa2c995b9f1cff4d468b0da49db
Yannik Ache Eicher <ache@bbit.io>
2023-01-16 10:26:35 +0100

    feat(deps): update to angular 15

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -120,7 +120,6 @@ export function appFactory(session: SessionService) {
     MatTableModule,
     MatRadioModule,
     MatListModule,
-    MatButtonModule,
     MatFormFieldModule,
   ],
   providers: [
