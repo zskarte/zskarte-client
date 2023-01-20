33fd8b88e71e951c31253715e96d405242c03f8b
Etienne Gobeli <gobeli@puzzle.ch>
2023-01-20 11:39:23 +0100

    wait on server before test

--- tests/global-setup.ts
+++ tests/global-setup.ts
@@ -11,7 +11,6 @@ async function globalSetup(config: FullConfig) {
   const { baseURL, storageState } = config.projects[0].use;
   const browser = await chromium.launch();
   const page = await browser.newPage({ baseURL });
-  page.setDefaultTimeout(90000);
   await login(page);
   await page.getByRole('button', { name: 'Neues Ereignis' }).click();
   await page.getByText('Bearbeiten').waitFor();
