e2313787ee128beffd47ebe8680530e9672fb8a8
Etienne Gobeli <gobeli@puzzle.ch>
2023-01-20 12:02:45 +0100

    try to fix tests

--- tests/global-setup.ts
+++ tests/global-setup.ts
@@ -8,7 +8,7 @@ export async function login(page: Page) {
 }
 
 async function globalSetup(config: FullConfig) {
-  const { baseURL, storageState } = config.projects[0].use;
+  const { baseURL } = config.projects[0].use;
   const browser = await chromium.launch();
   const page = await browser.newPage({ baseURL });
   await login(page);
@@ -16,8 +16,9 @@ async function globalSetup(config: FullConfig) {
   await page.getByText('Bearbeiten').waitFor();
   await page.getByPlaceholder('Name eingeben').fill('e2e test');
   await page.getByPlaceholder('Beschreibung eingeben').fill('e2e test');
-  await page.getByRole('button', { name: 'Speichern' }).click();
-  await page.context().storageState({ path: storageState as string });
+  const save = page.getByRole('button', { name: 'Speichern' });
+  await save.waitFor();
+  await save.click();
   await browser.close();
 
   // Teardown, remove operation
