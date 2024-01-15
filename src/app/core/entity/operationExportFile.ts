43f0b992148d7a403ce84e6809e6f9b68ec1fe8c
Etienne Gobeli <gobeli@puzzle.ch>
2024-01-15 10:36:45 +0100

    fix(import): #272 adds zsjson file V2

--- src/app/core/entity/operationExportFile.ts
+++ src/app/core/entity/operationExportFile.ts
@@ -2,6 +2,7 @@ import { IZsMapState } from '../../state/interfaces';
 
 export enum OperationExportFileVersion {
   V1 = 'V1',
+  V2 = 'V2',
 }
 
 // If you adjust this file you need to adjust the import as well
