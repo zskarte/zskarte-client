7bcfe104c32087053d4c260213d023044be9115f
Daniel Seifert <info@danielseifert.ch>
2024-01-16 13:19:01 +0100

    feat(rendering): Add RenderStrategy type and observable in map-renderer utils
    Introduced a new type `RenderStrategy` in the utils of the map-renderer to distinguish between 'webgl' and 'default' rendering methods. Also added an RxJS Subject `renderStrategy$` to broadcast the current rendering strategy. By doing this, other parts of the application can now react to changes in the map rendering strategy.

--- src/app/map-renderer/utils.ts
+++ src/app/map-renderer/utils.ts
@@ -1,6 +1,11 @@
 import CPUTileLayer from 'ol/layer/Tile';
 import GPUTileLayer from 'ol/layer/WebGLTile';
 import TileWMTS from 'ol/source/WMTS';
+import {Subject} from 'rxjs';
+
+export type RenderStrategy = 'webgl' | 'default';
+
+export const renderStrategy$ = new Subject<RenderStrategy>();
 
 function isWebGLSupported(): boolean {
   try {
@@ -16,9 +21,11 @@ export type OlTileLayerType = typeof CPUTileLayer<TileWMTS> | typeof GPUTileLaye
 function getOlTileLayer(): OlTileLayerType {
   if (isWebGLSupported()) {
     console.info('Using WebGL Tile Layer');
+    renderStrategy$.next('webgl');
     return GPUTileLayer;
   } else {
     console.info('Using Fallback Tile Layer');
+    renderStrategy$.next('default');
     return CPUTileLayer<TileWMTS>;
   }
 }
