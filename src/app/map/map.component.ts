9abed0ebb50f2643db2cd4657da4051230996d61
FreyCo11 <corentin.frey@unifr.ch>
2024-05-17 10:57:26 +0200

    Implemented offline warning to prevent login and dataloss

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { OfflineDialogComponent } from '../offline-dialog/offline-dialog.component';
 
 @Component({
   selector: 'app-map',
@@ -6,4 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./map.component.scss'],
 })
 // skipcq: JS-0327
-export class MapComponent {}
+export class MapComponent {
+
+  constructor(private dialog: MatDialog) {
+    localStorage.setItem("TriedReloading", "FALSE")
+  }
+
+
+  @HostListener('window:beforeunload', ['$event'])
+  handleBeforeUnload(event: BeforeUnloadEvent): void {
+
+    if (!navigator.onLine && window.localStorage.getItem("TriedReloading") === "FALSE") {
+      event.preventDefault();
+
+      const dialogRef = this.dialog.open(OfflineDialogComponent);
+
+      dialogRef.afterClosed().subscribe(confirmed => {
+        if (confirmed) {
+          localStorage.setItem("TriedReloading", "TRUE")
+          window.removeEventListener('beforeunload', this.handleBeforeUnload.bind(this));
+          window.location.assign(window.location.href)
+        }
+      });
+    }
+  }
+
+}
